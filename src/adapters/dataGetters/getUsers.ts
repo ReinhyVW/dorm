8b707059c3774ff86e0c3d78fdf4d031e4c22f88
Manuel Caballero <manu_cabarosa32@outlook.com>
2023-10-18 11:45:58 -0500

    Fixing environment variables

--- src/adapters/dataGetters/getUsers.ts
+++ src/adapters/dataGetters/getUsers.ts
@@ -7,7 +7,7 @@ export async function getUsers() {
 }
 
 export async function getUser(id: string) {
-  const res = await fetch(`${process.env.DB_HOST}api/users/${id}`, { cache: 'no-cache' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DB_HOST}api/users/${id}`, { cache: 'no-cache' });
 
   const user = await res.json();
 
@@ -15,7 +15,7 @@ export async function getUser(id: string) {
 }
 
 export async function getUserByEmail(email: string) {
-  const res = await fetch(`${process.env.DB_HOST}api/users/email/${email}`, { cache: 'no-cache' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_DB_HOST}api/users/email/${email}`, { cache: 'no-cache' });
 
   const user = await res.json();
 
