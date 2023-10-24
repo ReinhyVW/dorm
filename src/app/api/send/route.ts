32b3c8d7b464bdc7434d504c25860e879b849a34
ReinhyVW <manu_cabarosa32@outlook.com>
2023-10-23 23:51:03 -0500

    Checking Typoes

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,21 +1,19 @@
-import EmailTemplate from '@/components/email';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export async function POST(request: any) {
+export async function GET(request: any) {
   const data = await request.json();
 
-  console.log(data);
+  const { ReportedBy, AssignedTo, StatusId, CenterId, ActionDescription, Acuteness, ItemId } = await data;
 
   try {
     const data = await resend.emails.send({
       from: 'mcaballero@dhgmc.com',
       to: ['mcaballero@dhgmc.com'],
       subject: 'Hello world',
-      react: EmailTemplate({ ReportedBy: ReportedBy, AssignedTo: AssignedTo, StatusId: StatusId, CenterId: CenterId, ActionDescription: ActionDescription, Acuteness: Acuteness, ItemId: ItemId }),
-      text: ""
+      text: "Hola"
     });
 
     return NextResponse.json(data);
